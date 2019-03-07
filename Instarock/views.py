cbe40ba98f9760f6a04f28fc8e08a9863070eb62
Geerocktricks <geerockface4@gmail.com>
2019-03-08 00:11:27 +0300

    This commit adds the display day posted functionality

--- Instarock/views.py
+++ Instarock/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+import datetime as dt
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to the Moringa Tribune')
+    '''
+    function to display the welcome page
+    '''
+    date = dt.date.today()
+    html = f'''
+        <html>
+            <body>
+                <h1> {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(  html)
+
