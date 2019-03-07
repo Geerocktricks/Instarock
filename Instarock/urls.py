6c66a3199c6df4c8fb9474390ebfd5328bb4e6ab
Geerocktricks <geerockface4@gmail.com>
2019-03-08 00:30:27 +0300

    This commit adds the instarock app to the project settings

--- Instarock/urls.py
+++ Instarock/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_posts,name = 'pastNews')
 ]
--- Instarock/views.py
+++ Instarock/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http  import HttpResponse
+from django.http  import HttpResponse,Http404
 import datetime as dt
 # Create your views here.
 def welcome(request):
@@ -7,12 +7,52 @@ def welcome(request):
     function to display the welcome page
     '''
     date = dt.date.today()
+     # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
+    day = convert_dates(date)
     html = f'''
         <html>
             <body>
-                <h1> {date.day}-{date.month}-{date.year}</h1>
+                <h1>Posted on {day} {date.day}-{date.month}-{date.year}</h1>
             </body>
         </html>
             '''
     return HttpResponse(  html)
 
+def convert_dates(dates):
+    '''
+    Fubction to convert dates to days of the week
+    '''
+
+    # Function that gets the weekday number for the date.
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day
+
+def past_days_posts(request,past_date):
+    '''
+    Function to display past days posts
+    '''
+
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+    # Converts data from the string Url
+    date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>Posted on {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
