57c2549155b0107cd28f363ceecf3b6368d1fe11
Geerocktricks <geerockface4@gmail.com>
2019-03-11 16:16:03 +0300

    This commit installs the django registration

--- Instarock/urls.py
+++ Instarock/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
-    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_posts,name = 'pastNews')
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_posts,name = 'pastNews'),
+    url('signup/' , views.signup , name = 'signup'), 
 ]
